8719861160ef81fdb4efeba380fda89c08322203
Mario Alejandro Benitez Orozco <maalben@gmail.com>
2022-01-05 15:49:02 -0500

    Se finaliza ejercicio de automatizacion Mobile en iOS con Screenplay

---
+++ src/main/java/com/weelo/practicemobile/questions/TheAlert.java
@@ -0,0 +1,24 @@
+package com.weelo.practicemobile.questions;
+
+import com.weelo.practicemobile.models.TestData;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.questions.Text;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import static com.weelo.practicemobile.userinterfaces.AuthenticationUserInterfaces.ALERT_MESSAGE_ERROR;
+import static com.weelo.practicemobile.util.Constants.EXPECTED;
+import static com.weelo.practicemobile.util.Constants.TIME;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+
+public class TheAlert implements Question<String> {
+
+    @Override
+    public String answeredBy(Actor actor) {
+        actor.attemptsTo(WaitUntil.the(ALERT_MESSAGE_ERROR.of(TestData.getData().get(EXPECTED).toString()), isVisible()).forNoMoreThan(TIME).seconds());
+        return Text.of(ALERT_MESSAGE_ERROR.of(TestData.getData().get(EXPECTED).toString().trim())).viewedBy(actor).asString();
+    }
+
+    public static TheAlert message() {
+        return  new TheAlert();
+    }
+}
---
+++ src/test/java/com/weelo/practicemobile/stepdefinitions/AuthenticationStepDefinitios.java
@@ -0,0 +1,60 @@
+package com.weelo.practicemobile.stepdefinitions;
+
+import com.weelo.practicemobile.exceptions.AssertionsAutomationMobilepractice;
+import com.weelo.practicemobile.models.TestData;
+import com.weelo.practicemobile.questions.TheAlert;
+import com.weelo.practicemobile.questions.TheTitle;
+import com.weelo.practicemobile.tasks.Load;
+import com.weelo.practicemobile.tasks.Login;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.thucydides.core.annotations.Managed;
+
+import static com.weelo.practicemobile.util.Constants.EXPECTED;
+import static org.hamcrest.Matchers.is;
+import org.openqa.selenium.WebDriver;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import java.util.List;
+import java.util.Map;
+
+public class AuthenticationStepDefinitios {
+
+    @Managed(driver ="appium")
+    private WebDriver hisBrowser;
+    Actor mario = Actor.named("Mario");
+
+    @Before
+    public void setup()
+    {
+        mario.can(BrowseTheWeb.with(hisBrowser));
+    }
+
+    @Given("^I load test data$")
+    public void iLoadTestData(List<Map<String, String>> data) {
+        mario.wasAbleTo(Load.testData(data.get(0)));
+    }
+
+    @When("^I try to login myself using my data authentication$")
+    public void iTryToLoginMyselfUsingMyDataAuthentication(){
+        mario.attemptsTo(Login.inDemoApplicationSauceLabs());
+    }
+
+    @Then("^I should see the title screen expected$")
+    public void iShouldSeeTheTitleScreenExpected(){
+        mario.should(seeThat(TheTitle.is(), equalTo(true))
+                .orComplainWith(AssertionsAutomationMobilepractice.class,
+                        AssertionsAutomationMobilepractice.TITLE_ELEMENT_NO_EXPECTED));
+    }
+
+    @Then("^I should see the alert with message expected$")
+    public void iShouldSeeTheAlertWithMessageExpected(){
+        mario.should(seeThat(TheAlert.message(), is(TestData.getData().get(EXPECTED).toString()))
+                .orComplainWith(AssertionsAutomationMobilepractice.class,
+                        AssertionsAutomationMobilepractice.ALERT_MESSAGE_NO_EXPECTED));
+    }
+}
